cdab96d1d2e4b45976f9f7ff9d821b9806ba3200
樱落Eglo <452678184@qq.com>
2022-05-19 23:05:22 +0800

    [SW]Upload Test Program

---
+++ 2.Software/InertiaScreen/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\zhcne\AppData\Local\Temp\VMBuilds\InertiaScreen\esp8266_d1_mini\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ e:\microsoft\ visual\ studio\2022\community\common7\ide\extensions\2jjwldvd.zex\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
---
+++ 2.Software/InertiaScreen/InertiaScreen.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		InertiaScreen.ino
+ Created:	2022/5/19
+ Author:	YLEglo
+*/
+
+
+
+void setup() {
+	pinMode(D7, OUTPUT);
+}
+
+void loop() {
+	analogWrite(D7, 255);
+}
